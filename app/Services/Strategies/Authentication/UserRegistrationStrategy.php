c8f6b04752d4b2da0434d50faa7f5b6a71a93b04
Gabriel Angelo <gabriel_angelo100@hotmail.com>
2022-06-04 14:45:11 -0300

    .

---
+++ app/Services/Strategies/Authentication/UserRegistrationStrategy.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services\Strategies\Authentication;
+
+use App\Models\Game\Player;
+use App\Services\UserRegistrationServiceInterface;
+use Exception;
+
+class UserRegistrationStrategy implements \App\Services\UserRegistrationServiceInterface
+{
+    protected UserRegistrationServiceInterface $sender;
+
+    public function __construct(UserRegistrationServiceInterface $registrationService)
+    {
+        $this->sender = $registrationService;
+    }
+
+    /**
+     * @throws Exception
+     */
+    function register(array $payload): Player
+    {
+        return $this->sender->register($payload);
+    }
+
+    /**
+     * @throws Exception
+     */
+    function registerAndLogin(array $payload): ?string
+    {
+        return $this->sender->registerAndLogin($payload);
+    }
+}
